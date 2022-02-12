6f1d0008f7116f38e90b3a50c3650d684e0d8a69
ValiullinAd <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>
2022-02-12 17:42:44 +0300

    Initial commit

---
+++ src/test/java/testNg/BonusServiceTest.java
@@ -0,0 +1,18 @@
+package testNg;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class BonusServiceTest {
+
+    @Test
+    public void testCalculateBonus() {
+        BonusService bonusService = new BonusService();
+        int amount = 2000;
+        int expected = 10;
+        int actual = bonusService.calculateBonus(amount);
+        assertEquals(expected, actual, "Wrong bonus calc");
+
+    }
+}
